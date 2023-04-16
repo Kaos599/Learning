2276b563a010b8eb539298bf762c4b8945288efc
Kaos599 <115716485+Kaos599@users.noreply.github.com>
2023-04-16 22:56:44 +0530

    Create Celsius_to_fahrenheit.py

---
+++ python/Vityarthi/Module3/Celsius_to_fahrenheit.py
@@ -0,0 +1,10 @@
+"""Q.2 Convert the temperature in celsius to fahrenheit.
+Sample Input:
+Celsius = 30
+Sample Output: 86"""
+
+#Answer2:
+Celsius = float(input(“Please enter the temperature in fahrenheit: “))
+Fahrenheit = Celsius*(9/5) + 32
+print(Fahrenheit)
+

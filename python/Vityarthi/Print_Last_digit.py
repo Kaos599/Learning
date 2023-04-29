fa05bbcdf2efc9998fb2f4da003af06f05ca5508
Kaos599 <115716485+Kaos599@users.noreply.github.com>
2023-04-29 23:55:46 +0530

    Create Print_Last_digit.py

---
+++ python/Vityarthi/Print_Last_digit.py
@@ -0,0 +1,8 @@
+"""Q6. Print the last digit of the given number.
+Sample Input: 256
+Sample Output: 6"""
+
+Answer6: 
+a = int(input("Enter a number: "))
+last_digit = number % 10
+print("The last digit is:", last_digit)
